c566def183825ef0af26387c41ca34b8d9fff6bb
Mike Gieson <gieson007@gmail.com>
2013-08-02 08:48:24 -0400

    stand alone

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 domReady
 --------
+Small, standalone dom ready. No dependency on other libraries or frameworks (such as jquery). 
+
 It's easy. Works like this:
 
 ``` js
@@ -8,6 +10,9 @@ domready(function () {
 })
 ```
 
+Change the namespace from "domready" to "whatever" or incorporate into another object easily.
+
+
 Browser support
 ---------------
 
@@ -15,35 +20,63 @@ Browser support
   * Firefox 2+
   * Safari 3+
   * Chrome *
+  * Chromium *
   * Opera *
 
-Building
---------
+Tested within a wordpress site against the following browsers via BrowserStack.com
+	XP
+		IE 6+
+		Firefox 3+
+		Safari 4+
+		Chrome 14+
+		Opera 10+
 
-    $ npm install -dev
-    $ make
-    $ open tests/test.html
+	Win 7
+		IE 8+
+		Firefox 3+
+		Safari 4+
+		Chrome 14+
+		Opera 10+
 
-Including with Ender
---------------------
-Don't already have [Ender](http://ender.no.de)? Ender relies on [Node](http://nodejs.org), and [NPM](http://npmjs.org). Install it like this:
+	Win 8
+		IE 10
+		Firefox 16+
+		Safari 5.1
+		Chrome 22+
+		Opera 12+
+		
+	OS X 10.6 (Mountain Lion)
+		Firefox 3.6+
+		Safari 6
+		Chrome 14+
+		Opera 11+
+	
+	OS X 10.7 (Lion)
+		Firefox 3.6+
+		Safari 6
+		Chrome 14+
+		Opera 11+
+	
+	IOS (Simulator)
+		3.0 iPhone 3GS
+		5.1 iPhone 4S
+		6.0 iPhone 5
+		3.2 iPad 1
+		4.3 iPad 2
+		5.0 iPad 2
+		5.1 iPad 3
+		6.0 iPad 3
+		6.0 iPad Mini
+	
+	Android (Simulator)
+		2.2 Samsung Galaxy S
+		2.3 Samsung Galaxy S II
+		4.1 Samsung Galaxy S III
+		4.2 LG Nexus 4
+		
+	Opera Mobile (Simulator)
+		Motorola Droid X
+		Samsung Galaxy Tab 10.1
 
-    npm install ender -g
 
-Once you're good with that. Include domready in your package:
-
-    ender add domready
-
-Then use it like this:
-
-``` js
-$.domReady(function () {
-  $('body').html('<p>boosh</p>')
-})
-
-// or
-
-$(document).ready(function () {
-  $('body').html('<p>boosh</p>')
-})
 ```
--- domready.js
+++ domready.js
@@ -1,56 +1,125 @@
-var domready = function (readyFunc) {
+/*
+ * ====================
+ * Dom Ready
+ * ====================
+ * Modified (converted to singleton, commented and made the code readable by mortals)
+ * by Mike Gieson
+ * http://www.gieson.com
+ * https://github.com/bobtherobot/domready
+ * 
+ * Original (core) 
+ * by Dustin Diaz
+ * http://dustindiaz.com
+ * https://github.com/ded/domready
+*/
 
-	var ready, flush, fns = [], fn, f = false
-	, doc = document
-	, testEl = doc.documentElement
-	, hack = testEl.doScroll
-	, domContentLoaded = 'DOMContentLoaded'
-	, addEventListener = 'addEventListener'
-	, onreadystatechange = 'onreadystatechange'
-	, readyState = 'readyState'
-	, loadedRgx = hack ? /^loaded|^c/ : /^loaded|c/
-	, loaded = loadedRgx.test(doc[readyState]);
+
+(function() {
 	
+var ready = function () {
+
+	var user, flush, fns = [], fn, f = false
+	, doc 		= document
+	, testEl 	= doc.documentElement
+	, hack 		= testEl.doScroll
+	, domContentLoaded 		= 'DOMContentLoaded'
+	, addEventListener 		= 'addEventListener'
+	, onreadystatechange 	= 'onreadystatechange'
+	, readyState 			= 'readyState'
+	, loadedRgx = hack ? /^loaded|^c/ : /^loaded|c/
+	, loaded 	= loadedRgx.test(doc[readyState]);
+	
+	
+	// This will get called when the page is ready.
 	flush = function (f) {
+		
+		// Flag that we're ready
 		loaded = 1;
+		
+		// Remove each function, then call it... very tricky ded!
 		while (f = fns.shift()) {
 			f();
 		}
 	}
 	
+	// -------------
+	// W3C
+	// -------------
 	if(doc[addEventListener]){
+		
 		fn = function () {
-			flush();
+			
+			// Remove this function.
 			doc.removeEventListener(domContentLoaded, fn, f);
+			
+			// Run the functions.
+			flush();
+
 		}
+		
+		// Add the event listener
 		doc[addEventListener]( domContentLoaded, fn, f );
-						
-		ready = function (fref) {
+		
+		// This is what we'll expose for users to call.
+		user = function (fref) {
+			
+			// Call immediately or store until we flush.
 			loaded ? fref() : fns.push(fref);
 		}
+		
 	}
-
+	
+	// -------------
+	// MSIE
+	// -------------
 	if(hack){
+		
 		fn = function () {
+			
+			// document.readyState can be "loading", "loaded", "Complete", or "interaCtive"
+			// Test against "c"... very tricky ded!
 			if (/^c/.test(doc[readyState])) {
 				doc.detachEvent(onreadystatechange, fn);
 				flush();
 			}
 		}
+		
+		// Add the event listener - MSIE way.
 		doc.attachEvent( onreadystatechange, fn);
-		ready = function(fref){
+		
+		// This is what we'll expose for users to call.
+		user = function(fref){
+			
+			// Frames and iFrames honor onreadystatechange?
 			if(self != top){
 				loaded ? fref() : fns.push(fref);
+			
+			// ... whereas a normal page does not?
 			} else {
 				try {
+					
+					// Will produce errors until the document is completely loaded
 					testEl.doScroll('left');
 				} catch (e) {
-					return setTimeout(function() { ready(fref) }, 50);
+					
+					// ... so continue to poll until no errors.
+					return setTimeout(function() { user(fref) }, 50);
 				}
+				
+				// We'll only make it here if no errors.
 				fref();
 			}
 		}
+	
 	}
 	
-	return ready(readyFunc);
+	// Expose the "user" function for users to interact with.
+	return user;
+	
 }
+
+// Namespace. Change "domready" to whatever you think wont clash.
+// You can also change "window" to "this" (or whatever) to include 
+// within the context of another class or object.
+window.domready = ready();
+}());
